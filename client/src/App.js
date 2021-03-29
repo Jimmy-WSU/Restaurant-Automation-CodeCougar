7e5e7416ba7c00656ccfd54b51b9471ce3e0a24e
Jianqiao-WSU <jianqiao.liu@wsu.edu>
2021-03-29 18:47:51 +0800

    update

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,8 @@ function App() {
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-light">
         <div className="container">
-          <Link className="navbar-brand" to={"/homepage"}>Restaurant Automation</Link>
+          {/* <Link className="navbar-brand" to={"/homepage"}>Restaurant Automation</Link> */}
+            <Link className="navbar-brand">Restaurant Automation</Link>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
