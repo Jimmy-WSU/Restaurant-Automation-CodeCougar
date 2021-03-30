2e3447de0f95aea654403fc58003caaa2c53d2c7
Jimmy-WSU <wen-chih.li@wsu.edu>
2021-03-30 21:27:59 +0800

    update

--- test/signin.test.js
+++ test/signin.test.js
@@ -2,7 +2,7 @@ import { MemoryRouter } from 'react-router-dom'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { render, screen } from '@testing-library/react'
-import {Signin, button} from "../components/signin.component";
+import {Signin, button} from "../client/src/components/signin.component";
 import { shallow } from 'enzyme';
 
 
