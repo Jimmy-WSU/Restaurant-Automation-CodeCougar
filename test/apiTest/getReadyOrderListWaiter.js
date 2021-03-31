ced4c937adff860f46149c49610f7db7a2a21c45
Parikshit-Panwar-WSU <77930781+Parikshit-Panwar-WSU@users.noreply.github.com>
2021-03-31 18:47:29 +0530

    Add files via upload
    Order Testing

---
+++ test/apiTest/getReadyOrderListWaiter.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+const qs = require('qs');
+let data = qs.stringify({
+   
+});
+let config = {
+  method: 'post',
+  url: 'http://localhost:3001/getReadyOrderList',
+  headers: { },
+  data : data
+};
+
+axios(config)
+.then((response) => {
+  console.log(JSON.stringify(response.data));
+})
+.catch((error) => {
+  console.log(error);
+});
+
+//Postman test script, can only run in Postman
+pm.test("Get ready order list sucessfully", function () {
+  var jsonData = pm.response.json();
+  console.log(jsonData);
+  pm.expect(jsonData.status).to.eql('Successful');
+});
+
+pm.test("Get Order Details", function () {
+  pm.expect(pm.response.text()).to.include("orderDetails");
+});
