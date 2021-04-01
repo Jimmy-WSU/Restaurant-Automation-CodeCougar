53ca3fd2a7fe66980074806ac48acca81dbd9e57
Jianqiao-WSU <jianqiao.liu@wsu.edu>
2021-04-01 16:21:23 +0800

    update database

--- server/sql/restaurant_automation.sql
+++ server/sql/restaurant_automation.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80023
  File Encoding         : 65001
 
- Date: 12/03/2021 23:22:55
+ Date: 01/04/2021 16:15:25
 */
 
 SET NAMES utf8mb4;
@@ -27,7 +27,7 @@ CREATE TABLE `foodmenu`  (
   `foodprice` decimal(10, 2) NOT NULL,
   `sale` int NULL DEFAULT NULL,
   PRIMARY KEY (`key`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 20 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of foodmenu
@@ -36,7 +36,7 @@ INSERT INTO `foodmenu` VALUES (1, 'Hamburger', 1.00, 1000);
 INSERT INTO `foodmenu` VALUES (2, 'Steak', 9.99, 100);
 INSERT INTO `foodmenu` VALUES (3, 'French fries', 1.00, 500);
 INSERT INTO `foodmenu` VALUES (4, 'Salad', 5.00, 100);
-INSERT INTO `foodmenu` VALUES (5, 'Dumplings', 2.99, 40);
+INSERT INTO `foodmenu` VALUES (5, 'Dumplings', 2.99, 100);
 
 -- ----------------------------
 -- Table structure for order
@@ -52,15 +52,20 @@ CREATE TABLE `order`  (
   `orderStatus` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `createTime` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`orderID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 52 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 67 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of order
 -- ----------------------------
 INSERT INTO `order` VALUES (53, 2, 'Steven', 'TomChef', 'Steak, French fries', 10.99, 'Finished', '2021-03-12 22:59:28');
-INSERT INTO `order` VALUES (54, 3, 'SteveWaiter', 'TomChef', 'Steak, French fries', 10.99, 'Ready', '2021-03-12 22:43:49');
-INSERT INTO `order` VALUES (55, 5, 'SteveWaiter', 'Unassigned', 'Hamburger, Steak, French fries, Salad, Dumplings', 19.98, 'Started', '2021-03-12 22:43:27');
-INSERT INTO `order` VALUES (56, 4, 'SteveWaiter', 'Unassigned', 'Steak, French fries', 10.99, 'Started', '2021-03-12 23:04:49');
+INSERT INTO `order` VALUES (58, 6, 'SteveWaiter', 'TomChef', 'Hamburger, Steak, French fries, Salad, Dumplings', 19.98, 'Finished', '2021-03-30 14:09:04');
+INSERT INTO `order` VALUES (60, 2, 'SteveWaiter', 'TomChef', 'Hamburger, Steak, French fries, Salad', 16.99, 'Finished', '2021-03-30 14:10:02');
+INSERT INTO `order` VALUES (61, 3, 'SteveWaiter', 'TomChef', 'Hamburger, Steak, French fries, Salad, Dumplings', 19.98, 'Finished', '2021-03-30 20:30:51');
+INSERT INTO `order` VALUES (63, 2, 'SteveWaiter', 'TomChef', 'Hamburger, Steak, French fries, Salad, Dumplings', 19.98, 'Finished', '2021-03-30 20:30:55');
+INSERT INTO `order` VALUES (64, 5, 'SteveWaiter', 'TomChef', 'Hamburger, Steak, French fries', 11.99, 'Ready', '2021-03-30 20:30:05');
+INSERT INTO `order` VALUES (65, 3, 'SteveWaiter', 'TomChef', 'Hamburger, Steak, French fries', 11.99, 'Finished', '2021-03-31 21:12:37');
+INSERT INTO `order` VALUES (66, 1, 'SteveWaiter', 'TomChef', 'Hamburger, Steak, French fries, Salad, Dumplings', 19.98, 'Cooking', '2021-03-31 20:55:29');
+INSERT INTO `order` VALUES (68, 6, 'SteveWaiter', 'Unassigned', 'Hamburger, Steak, French fries, Salad, Dumplings, Pizza', 30.97, 'Started', '2021-03-31 22:43:27');
 
 -- ----------------------------
 -- Table structure for table
@@ -75,13 +80,13 @@ CREATE TABLE `table`  (
 -- ----------------------------
 -- Records of table
 -- ----------------------------
-INSERT INTO `table` VALUES (1, 'Free');
-INSERT INTO `table` VALUES (2, 'Dirty');
-INSERT INTO `table` VALUES (3, 'Busy');
+INSERT INTO `table` VALUES (1, 'Busy');
+INSERT INTO `table` VALUES (2, 'Free');
+INSERT INTO `table` VALUES (3, 'Free');
 INSERT INTO `table` VALUES (4, 'Busy');
 INSERT INTO `table` VALUES (5, 'Busy');
-INSERT INTO `table` VALUES (6, 'Free');
-INSERT INTO `table` VALUES (7, 'Free');
+INSERT INTO `table` VALUES (6, 'Busy');
+INSERT INTO `table` VALUES (7, 'Busy');
 
 -- ----------------------------
 -- Table structure for user
@@ -105,5 +110,6 @@ INSERT INTO `user` VALUES ('BobManager', '123456', 'Manager', '2021-03-12 22:31:
 INSERT INTO `user` VALUES ('JamesBusboy', '123456', 'Busboy', '2021-03-12 22:31:06', NULL, 'Steve', 'Jobs');
 INSERT INTO `user` VALUES ('SteveWaiter', '123456', 'Waiter', '2021-03-12 22:30:59', NULL, 'Steve', 'Jobs');
 INSERT INTO `user` VALUES ('TomChef', '123456', 'Chef', '2021-03-12 22:31:12', NULL, 'Steve', 'Jobs');
+INSERT INTO `user` VALUES ('WadeWaiter', '123456', 'Waiter', NULL, NULL, 'Dwyane', 'Wade');
 
 SET FOREIGN_KEY_CHECKS = 1;
